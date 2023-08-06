b032737e49d6b06e7185e48261dde4bae53aa8f2
Aleksey Kuzmin <kboomska@gmail.com>
2023-08-06 15:07:00 +0300

    Refactoring main.dart (firebase)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:weather_service/src/common/dependencies/di_container.dart';
 import 'package:weather_service/src/common/bloc/app_bloc_observer.dart';
+import 'package:weather_service/firebase_options.dart';
 
 abstract interface class IAppFactory {
   Widget makeApp();
@@ -11,7 +13,12 @@ abstract interface class IAppFactory {
 
 final appFactory = makeAppFactory();
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Bloc.observer = AppBlocObserver();
 
   final app = appFactory.makeApp();
