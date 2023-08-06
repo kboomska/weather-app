7e18c6af7f952c725312c3ce1f6f8cebe4937a03
Aleksey Kuzmin <kboomska@gmail.com>
2023-08-06 18:15:05 +0300

    Refactoring LoginState

--- lib/src/feature/login/bloc/login_state.dart
+++ lib/src/feature/login/bloc/login_state.dart
@@ -1,41 +1,44 @@
-import 'package:meta/meta.dart';
-
-import 'package:weather_service/src/feature/login/model/user.dart';
+part of 'package:weather_service/src/feature/login/bloc/login_bloc.dart';
 
 sealed class LoginState extends _$LoginStateBase {
-  const LoginState({required super.user});
+  const LoginState({
+    required super.email,
+    required super.password,
+  });
 
   const factory LoginState.idle({
-    required User user,
+    required String email,
+    required String password,
     String? error,
   }) = _LoginState$Idle;
 
   const factory LoginState.processing({
-    required User user,
+    required String email,
+    required String password,
   }) = _LoginState$Processing;
 
   static const LoginState initialState = LoginState.idle(
-    user: User(
-        idToken: null,
-        email: null,
-        refreshToken: null,
-        expiresIn: null,
-        localId: null,
-        registered: null,
-        displayName: null),
-    error: null,
+    email: '',
+    password: '',
   );
 }
 
 final class _LoginState$Idle extends LoginState {
-  const _LoginState$Idle({required super.user, this.error});
+  const _LoginState$Idle({
+    required super.email,
+    required super.password,
+    this.error,
+  });
 
   @override
   final String? error;
 }
 
 final class _LoginState$Processing extends LoginState {
-  const _LoginState$Processing({required super.user});
+  const _LoginState$Processing({
+    required super.email,
+    required super.password,
+  });
 
   @override
   String? get error => null;
@@ -43,11 +46,13 @@ final class _LoginState$Processing extends LoginState {
 
 @immutable
 abstract base class _$LoginStateBase {
-  final User user;
+  final String email;
+  final String password;
   abstract final String? error;
 
   const _$LoginStateBase({
-    required this.user,
+    required this.email,
+    required this.password,
   });
 
   bool get hasError => error != null;
@@ -57,10 +62,11 @@ abstract base class _$LoginStateBase {
     if (identical(this, other)) return true;
 
     return other is _$LoginStateBase &&
-        other.user == user &&
+        other.email == email &&
+        other.password == password &&
         other.error == error;
   }
 
   @override
-  int get hashCode => user.hashCode ^ error.hashCode;
+  int get hashCode => email.hashCode ^ password.hashCode ^ error.hashCode;
 }
